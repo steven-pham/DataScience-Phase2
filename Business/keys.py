7a483d2cc1eade3eee5d58cffcf65e50b0b761c1
Steven Pham <52262343+steven-pham@users.noreply.github.com>
2020-09-19 12:21:24 +1200

    :tired_face:

---
+++ Business/keys.py
@@ -0,0 +1,5 @@
+#These keys are examples only, they won't work.
+#This is what the keys file should contain.
+client_id='5beanXBxD9vSbQ'
+client_secret='IMXAtF_44Ez0K2-9EWRNXf4J4cc'
+username='steven861998'
